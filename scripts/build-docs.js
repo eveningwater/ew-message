f78aa522bf9dfb81a847dcf44c125877f3a904c7
eveningwater <854806732@qq.com>
2023-10-04 11:14:26 +0800

    feat: 修改了文档

--- scripts/build-docs.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-if (!fs.existsSync('gh-pages')) {
-  fs.mkdirSync('gh-pages');
-}
--- typings/warn.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const MESSAGE_WARNING_PREFIX = "[Message Warning]: ";
-export declare const MESSAGE_TYPE_WARNING: string;
-export declare const MESSAGE_CLOSE_PARAM_WARNING: string;
-export declare const MESSAGE_CONTENT_PARAM_WARNING: string;
