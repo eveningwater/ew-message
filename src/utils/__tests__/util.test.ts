e07ef83453e97f3a6e51617ea525e4c7ec1e44ae
eveningwater <854806732@qq.com>
2024-10-31 03:41:07 +0000

    feat: 添加了新的单元测试

--- src/utils/__tests__/util.test.ts
+++ src/utils/__tests__/util.test.ts
@@ -1,4 +1,6 @@
-import { hasOwn, isObject } from "../util";
+import { hasOwn, isDom, isNumber, isObject, isString, isUndef } from "../util";
+
+
 
 describe('utils', () => {
   test('hasOwn', () => {
@@ -9,5 +11,53 @@ describe('utils', () => {
     expect(isObject({ a: 1 })).toBe(true);
     expect(isObject([])).toBe(true);
     expect(isObject(null)).toBe(false);
+  });
+  test('isUndef', () => {
+    expect(isUndef(undefined)).toBe(true);
+    expect(isUndef(null)).toBe(false);
+    expect(isUndef([])).toBe(false);
+    expect(isUndef(true)).toBe(false);
+    expect(isUndef({})).toBe(false);
+    expect(isUndef('')).toBe(false);
+    expect(isUndef(123)).toBe(false);
+  })
+
+  test('isString', () => {
+    expect(isString('')).toEqual(true);
+    expect(isString(1)).toEqual(false);
+  })
+
+  test('isNumber', () => {
+    expect(isNumber(1)).toEqual(true);
+    expect(isNumber(1e2)).toEqual(true);
+    expect(isNumber(NaN)).toEqual(false);
+    expect(isNumber('123')).toEqual(false);
+  })
+
+
+  describe('isDom', () => {
+    jest.mock('../util.ts', {
+      // @ts-ignore
+      isObject: jest.fn((v) => isObject(v))
+    });
+    test('should return true for HTMLElement', () => {
+      const element = document.createElement('div');
+      expect(isDom(element)).toBe(true);
+    });
+
+    test('should return true for HTMLCollection', () => {
+      const collection = document.getElementsByTagName('div');
+      expect(isDom(collection)).toBe(true);
+    });
+
+    test('should return true for NodeList', () => {
+      const list = document.querySelectorAll('div');
+      expect(isDom(list)).toBe(true);
+    });
+
+    test('should return false for non-HTML element', () => {
+      const nonElement = {};
+      expect(isDom(nonElement)).toBe(false);
+    });
   });
 });    
