bc77c84f961a513cf775313cc6118e199ff7483a
eveningwater <854806732@qq.com>
2024-11-03 07:38:27 +0000

    fix: 调整了官网跳转示例逻辑

--- docs/src/components/demo/index.tsx
+++ docs/src/components/demo/index.tsx
@@ -1,40 +1,40 @@
-import { useHistory } from "@docusaurus/router";
+import { openNewWindow } from "@site/src/utils";
 
 const demoList = [
     {
-        title:"非框架示例",
-        src: "/ew-message/demo.html"
+        title: "非框架示例",
+        src: "/demo.html"
     },
     {
-        title:"react框架示例",
-        src: "/ew-message/react-demo/index.html"
+        title: "react框架示例",
+        src: "/react-demo/index.html"
     },
     {
-        title:"vue框架示例",
-        src: "/ew-message/vue-demo/index.html"
+        title: "vue框架示例",
+        src: "/vue-demo/index.html"
     }
 ]
 export default function Demo(): JSX.Element {
-    const history = useHistory();
-    const onOpenHandler  = (src: string) => {
-        history.push(src);
+    const onOpenHandler = (src: string) => {
+        const href = process.env.NODE_ENV !== "development" ? `${location.href}${src}` : `https://eveningwater.github.io/ew-message/${src}`
+        openNewWindow(href)
     }
     return (
-     <>
-        {
-            demoList.map((item) => {
-                return (
-                    <button
-                    className="button button--secondary button--lg"
-                    onClick={() => onOpenHandler(item.src)}
-                    style={{ marginLeft: 15 }}
-                    key={item.title}
-                  >
-                    查看{ item.title }
-                  </button>
-                )
-            })
-        }
-     </>
+        <>
+            {
+                demoList.map((item) => {
+                    return (
+                        <button
+                            className="button button--secondary button--lg"
+                            onClick={() => onOpenHandler(item.src)}
+                            style={{ marginLeft: 15 }}
+                            key={item.title}
+                        >
+                            查看{item.title}
+                        </button>
+                    )
+                })
+            }
+        </>
     );
-  }
+}
