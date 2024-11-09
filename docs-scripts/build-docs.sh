6d45d8838d8964da25020d4b4de9eb5808d5fd1b
eveningwater <854806732@qq.com>
2024-11-09 08:49:19 +0000

    feat: 添加了脚本文件

---
+++ docs-scripts/build-docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 执行构建文档相关任务
+npm run build:docs
+
+# 执行构建 demo 相关任务
+npm run build:demo
+
+# 执行构建 React demo 相关任务
+npm run build:react-demo
+
+# 执行构建 Vue demo 相关任务
+npm run build:vue-demo
+
+# 执行发布文档任务
+npm run release-docs
