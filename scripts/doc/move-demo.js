20a1f0f419bf1f9d5707cdda6efed1c7eab8bfb7
eveningwater <854806732@qq.com>
2025-04-02 06:28:02 +0000

    feat: 添加了playground

--- scripts/doc/move-demo.js
+++ scripts/doc/move-demo.js
@@ -4,27 +4,34 @@ const path = require('path');
 const sourceDir = 'examples/demo/';
 const targetDir = 'gh-pages'; 
 
-if (!fs.existsSync(targetDir)) {
-    fs.mkdirSync(targetDir, { recursive: true });
+const moveFunction = (sourceDir, targetDir) => {
+    if (!fs.existsSync(targetDir)) {
+        fs.mkdirSync(targetDir, { recursive: true });
+    }
+    fs.readdir(sourceDir, (err, files) => {
+        if (err) {
+            return console.error(`无法读取目录: ${err}`);
+        }
+    
+        files.forEach(file => {
+            const filePath = path.join(sourceDir, file);
+            if (path.extname(file) === '.html' || path.extname(file) === '.css' || path.extname(file) === '.js') {
+                const targetPath = path.join(targetDir, file);
+                // rename是移动
+                fs.copyFile(filePath, targetPath, (err) => {
+                    if (err) {
+                        return console.error(`无法移动文件 ${file}: ${err}`);
+                    }
+                    console.log(`成功移动文件: ${file} 到 ${targetDir}`);
+                });
+            }
+        });
+    });
+    
 }
 
-fs.readdir(sourceDir, (err, files) => {
-    if (err) {
-        return console.error(`无法读取目录: ${err}`);
-    }
+// 移动demo
+moveFunction(sourceDir, targetDir);
+// 复制playground
+moveFunction('playground/', `${targetDir}/playground`);
 
-    files.forEach(file => {
-        const filePath = path.join(sourceDir, file);
-
-        if (path.extname(file) === '.html') {
-            const targetPath = path.join(targetDir, file);
-            // rename是移动
-            fs.copyFile(filePath, targetPath, (err) => {
-                if (err) {
-                    return console.error(`无法移动文件 ${file}: ${err}`);
-                }
-                console.log(`成功移动文件: ${file} 到 ${targetDir}`);
-            });
-        }
-    });
-});
